ca7e0e014668f435745ce8e43f943f400194a8f0
Renu Yadav <renuya886@gmail.com>
2025-03-24 21:25:07 +0530

    yadav

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
-import './App.css'
-import Home from './Pages/Home'
-import { BrowserRouter as Router, Routes,Route } from 'react-router-dom'
-import './Styles/global.css'
-import Userprofile from './Pages/Userprofile'
-import CreateProfile from './Pages/CreateProfile'
-import Login from './Pages/login'
+import './App.css';
+import Home from './Pages/Home';
+import { BrowserRouter as Router, Routes,Route } from 'react-router-dom';
+import './Styles/global.css';
+import Userprofile from './Pages/Userprofile';
+import CreateProfile from './Pages/CreateProfile';
+import Login from './Pages/Login';
 
 function App() {
 

31fa7609b72bc5d46f6c4069c0179d986d28a298
Renu Yadav <renuya886@gmail.com>
2025-03-24 21:13:56 +0530

    sbk

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,9 @@ import './App.css'
 import Home from './Pages/Home'
 import { BrowserRouter as Router, Routes,Route } from 'react-router-dom'
 import './Styles/global.css'
-import Login from './Pages/login'
 import Userprofile from './Pages/Userprofile'
 import CreateProfile from './Pages/CreateProfile'
+import Login from './Pages/login'
 
 function App() {
 
